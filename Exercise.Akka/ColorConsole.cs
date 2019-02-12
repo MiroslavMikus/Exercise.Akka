61975d1c594d44024711b3e818b1bf34a71bf232
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-02-12 06:42:10 +0100

    Add akka.consoles tests

---
+++ Exercise.Akka/ColorConsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Akka
+{
+    public static class ColorConsole
+    {
+        public static void WriteGreen(string message) =>
+            WriteWithColor(message, ConsoleColor.Green);
+
+        public static void WriteYelow(string message) =>
+            WriteWithColor(message, ConsoleColor.Yellow);
+
+        public static void WriteWithColor(string message, ConsoleColor color)
+        {
+            var before = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = before;
+        }
+    }
+}
---
+++ Exercise.Akka/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Actor.Internal;
+using Exercise.Akka.Actors;
+using Exercise.Akka.Messages;
+
+namespace Exercise.Akka
+{
+    class Program
+    {
+        private static ActorSystem StreamingActorSystem;
+        static void Main(string[] args)
+        {
+            StreamingActorSystem = ActorSystem.Create("StreamingActorSystem");
+
+            Console.WriteLine("Actor system was created");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+
+            IActorRef actorRef = StreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            actorRef.Tell(new PlayMessage("The title", 42));
+            actorRef.Tell(new PlayMessage("Play this", 3));
+            actorRef.Tell(new PlayMessage("And play that", 643));
+
+            //actorRef.Tell(42);
+
+            //actorRef.Tell(4D);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Disposing actor system");
+
+            StreamingActorSystem.Dispose();
+
+            Console.WriteLine("Disposed");
+
+            Console.ReadLine();
+        }
+    }
+}
